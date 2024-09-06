fbb72cc3fef44159e9f1c22d24aadb6e6f74e597
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-09-06 03:31:02 +0000

    sum of  n

---
+++ CPP QUESTIONS /sum_of_n.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+// #include<conio.h>
+using namespace std; 
+int main (){
+    int n,t=0;
+    cout << "how much number of sum you need "<<endl;
+    cin>>n;
+    for(int i =1; i <=n;i++){
+     t=t+i;
+    }
+    cout<<"sum of n number is "<<t;
+}
+
+
+
+
