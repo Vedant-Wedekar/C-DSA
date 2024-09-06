90a96e6f16280cd9419bcc8b372f66ca17097f60
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-09-06 03:13:18 +0000

    simple area of square

---
+++ CPP QUESTIONS /AOS.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+// #include<conio.h>
+using namespace std;
+
+int main (){
+    int s;
+    cout <<"enter your side of square";
+    cin >>s;
+
+    cout <<"Area of Square is = "<<s*s;
+    return 0 ; 
+}
+
