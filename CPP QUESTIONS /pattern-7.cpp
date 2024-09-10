4aa2e9fcc45cddc59dc8a708957ce4c684235072
Vedant-Wedekar <vedantwedekar@gmail.com>
2024-09-10 09:43:46 +0000

    pattern question

---
+++ CPP QUESTIONS /pattern-7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ; 
+int main (){
+    int n;
+    cout<<"enter number ";
+    cin >>n ;
+    for (int i = 0 ; i <= n ; i++){
+        for(int j =0 ; j<i+1;j++){
+            cout<<i+1;
+        }
+        cout<<endl;
+    }
+    return 0 ; 
+}
